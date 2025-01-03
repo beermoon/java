e2ebf2101a57eb75b0582f932ac858ffe711ccf8
unknown <featermoon0318@gamil.com>
2025-01-03 17:44:25 +0900

    variable

---
+++ test/src/ch01/sec10/Hello.java
@@ -0,0 +1,13 @@
+package ch01.sec10;
+
+public class Hello {
+	//프로그램 실행 진입점
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello Java");
+		
+	}
+
+}
