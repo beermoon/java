4b54825c83fcae9cce721da58a17f8d17d3228a7
unknown <featermoon0318@gamil.com>
2025-01-14 17:48:46 +0900

    백준

---
+++ codingTest/src/level1/N_11382.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Scanner;
+
+public class N_11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a,b,c;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		System.out.printf("%d", a+b+c);
+		
+		
+		
+	}
+}
--- codingTest/src/level1/N_2588.java
+++ codingTest/src/level1/N_2588.java
@@ -1,5 +1,20 @@
 package level1;
 
-public class N_2588 {
+import java.util.Scanner;
 
+public class N_2588 {
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			int a,b;
+			
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			System.out.println((b % 10) * a );
+			System.out.println(((b / 10) % 10) * a);	// 10의 자리
+			System.out.println((b / 100)  * a);
+			System.out.println(a * b);
+		}
 }
